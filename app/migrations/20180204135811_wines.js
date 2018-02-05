6be5dc4304a0d003fb90b8d2ab067a2b2251e165
Paige Throckmorton <path4942@gmail.com>
2018-02-05 12:38:30 -0600

    Redid migrations, tables set for seeding

--- app/migrations/20180204135811_wines.js
+++ app/migrations/20180204135811_wines.js
@@ -2,12 +2,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('wines', function(table){
     table.increments('id');
-    table.integer('wine_com_id');
     table.string('name');
     table.string('varietal');
+    table.string('origin');
     table.string('description');
     table.float('price_retail');
-    table.string('url');
     table.integer('vintage');
   })
 };
--- app/migrations/20180204140632_join-tables.js
+++ app/migrations/20180204140632_join-tables.js
@@ -9,8 +9,15 @@ exports.up = function(knex, Promise) {
   .then(function(){
     return knex.schema.createTableIfNotExists('food_pairings', function(table){
       table.increments('id').notNullable();
+      table.integer('cuisine_id').references('cuisine_lists.id').onDelete('cascade');
+      table.string('food');
       table.integer('wine_id').references('wines.id').onDelete('cascade');
-      table.string('item')
+    })
+  })
+  .then(function(){
+    return knex.schema.createTableIfNotExists('menu_pairings', function(table){
+      table.integer('user_id').references('users.id').onDelete('cascade');
+      table.integer('pairing_id').references('food_pairings.id').onDelete('cascade');
     })
   })
 };
@@ -19,5 +26,8 @@ exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('wine_lists')
   .then(function(){
     return knex.schema.dropTableIfExists('food_pairings')
+  })
+  .then(function(){
+    return knex.schema.dropTableIfExists('menu_pairings')
   })
 };
