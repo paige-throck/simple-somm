e659d8c25bc6121af91138cc814a26753805ef6a
Paige Throckmorton <path4942@gmail.com>
2018-02-06 07:54:56 -0600

    merging branch before getting started

--- app/routes/pairings.js
+++ app/routes/pairings.js
@@ -3,5 +3,18 @@ const router = express.Router();
 //this may need to change depending on what we do with the db file
 const knex = require('../db');
 
+//get all wines for the search function----------------------
+router.get('/', function(req, res){
+
+//knex logic
+
+});
+
+//get a single wine------------
+router.get('/:id', function(req, res){
+  const id = req.params.id;
+  //knex logic
+
+})
 
 module.exports = router;
